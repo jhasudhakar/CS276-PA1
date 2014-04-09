60fd92d5d73fb397bf174828fdad8fb1f4e7f8ef
Kavin Yao <kavinyao@gmail.com>
2014-04-09 11:16:41 -0700

    fix potential integer overflow

--- cs276-pa1/src/cs276/util/TermDocComparator.java
+++ cs276-pa1/src/cs276/util/TermDocComparator.java
@@ -8,16 +8,19 @@ import java.util.Comparator;
  * @author Wei Wei
  */
 public class TermDocComparator implements Comparator<Pair<Integer, Integer>> {
-    public int compare(Pair<Integer, Integer> x, Pair<Integer, Integer> y) {
-        int xFirst = x.getFirst();
-        int xSecond = x.getSecond();
-        int yFirst = y.getFirst();
-        int ySecond = y.getSecond();
+    // why not simply use i - j?
+    // see http://www.cpylua.org/root-of-all-evil/
+    private int compareInt(int i, int j) {
+        return i == j ? 0 : (i < j ? -1 : 1);
+    }
 
-        if (xFirst == yFirst) {
-            return xSecond - ySecond;
-        } else {
-            return xFirst - yFirst;
-        }
+    public int compare(Pair<Integer, Integer> x, Pair<Integer, Integer> y) {
+        int termID1 = x.getFirst();
+        int docID1 = x.getSecond();
+        int termID2 = y.getFirst();
+        int docID2 = y.getSecond();
+
+        int res = compareInt(termID1, termID2);
+        return res != 0 ? res : compareInt(docID1, docID2);
     }
 }
