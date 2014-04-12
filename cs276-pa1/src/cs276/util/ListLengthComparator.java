16237661ec9beb3332c8296c162866f8f75cc628
Wei Wei <fraternite@live.cn>
2014-04-12 14:58:33 -0700

    modify type of ListLengthComparator

--- cs276-pa1/src/cs276/util/ListLengthComparator.java
+++ cs276-pa1/src/cs276/util/ListLengthComparator.java
@@ -1,19 +1,18 @@
 package cs276.util;
 
-import cs276.assignments.PostingList;
-
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * Created by weiwei on 4/12/14.
  */
-public class ListLengthComparator implements Comparator<PostingList> {
+public class ListLengthComparator implements Comparator<List<Integer>> {
     private int compareInt(int i, int j) {
         return i == j ? 0 : (i < j ? -1 : 1);
     }
-    public int compare(PostingList p1, PostingList p2) {
-        int p1len = p1.getList().size();
-        int p2len = p2.getList().size();
-        return compareInt(p1len, p2len);
+    public int compare(List<Integer> l1, List<Integer> l2) {
+        int len1 = l1.size();
+        int len2 = l2.size();
+        return compareInt(len1, len2);
     }
 }
