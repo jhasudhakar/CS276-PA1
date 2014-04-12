8c48d74caf587db12fcc9e8725edc59056b8af68
Wei Wei <fraternite@live.cn>
2014-04-12 14:58:52 -0700

    framework finished.

--- cs276-pa1/src/cs276/assignments/Query.java
+++ cs276-pa1/src/cs276/assignments/Query.java
@@ -1,5 +1,7 @@
 package cs276.assignments;
 
+import cs276.util.ListLengthComparator;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -7,8 +9,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Query {
 
@@ -37,6 +38,68 @@ public class Query {
             return index.readPosting(fc.position(posDict.get(termId)));
         }
         return null;
+    }
+
+    /**
+     * Pop next element if there is one, otherwise return null
+     * @param p iterator
+     * @param <X> class
+     * @return next element or null
+     */
+    static <X> X popNextOrNull(Iterator<X> p) {
+        if (p.hasNext()) {
+            return p.next();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Intersect two posting lists
+     * @param p1
+     * @param p2
+     * @return the intersection of two posting lists
+     */
+    static List<Integer> intersect(Iterator<Integer> p1, Iterator<Integer> p2) {
+        List<Integer> answer = new ArrayList<Integer>();
+        Integer docId1 = popNextOrNull(p1);
+        Integer docId2 = popNextOrNull(p2);
+
+        while (docId1 != null && docId2 != null) {
+            if (docId1 == docId2) {
+                answer.add(docId1);
+                docId1 = popNextOrNull(p1);
+                docId2 = popNextOrNull(p2);
+            }
+            else if (docId1 < docId2) {
+                docId1 = popNextOrNull(p1);
+            } else {
+                docId2 = popNextOrNull(p2);
+            }
+        }
+
+        return answer;
+    }
+
+    /**
+     * Output query results to stdout
+     * @param res
+     */
+    static void outputQueryResult(List<Integer> res) {
+        if (res == null || res.size() == 0) {
+            System.out.println("no results found");
+            return;
+        }
+        List<String> output = new ArrayList<String>();
+        for (Integer i : res) {
+            //document file name should only include subdirectory name and then the file name
+            assert(docDict.containsKey(i));
+            output.add(docDict.get(i));
+        }
+        Collections.sort(output);
+        for (String s : output) {
+            System.out.println(s);
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -103,10 +166,36 @@ public class Query {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         /* For each query */
+        Boolean termNotFound = false;
         while ((line = br.readLine()) != null) {
-            /*
-             * Your code here
-             */
+            // tokens in the query are separated by space
+            String[] tokens = line.split("\\s+");
+            // get posting list for each query word
+            List<List<Integer>> postingLists = new ArrayList<List<Integer>>();
+            for (String token : tokens) {
+                if (termDict.containsKey(token)) {
+                    postingLists.add(readPosting(indexFile.getChannel(), termDict.get(token)).getList());
+                } else {
+                    // if the query word is not indexed, simply return "no results found"
+                    termNotFound = true;
+                }
+            }
+            if (!termNotFound) {
+                termNotFound = false;
+                System.out.println("no results found");
+                continue;
+            }
+            // order the terms by postings list length to optimize query performance
+            Collections.sort(postingLists, new ListLengthComparator());
+            // posting list intersection
+            Iterator<List<Integer>> iter = postingLists.iterator();
+            List<Integer> pl1 = popNextOrNull(iter), pl2;
+            while (pl1 != null && (pl2 = popNextOrNull(iter)) != null) {
+                // intersect two posting lists
+                pl1 = intersect(pl1.iterator(), pl2.iterator());
+            }
+            // output result
+            outputQueryResult(pl1);
         }
         br.close();
         indexFile.close();
