3b1bc212e776613b996a57a369c2bba676105afc
Wei Wei <fraternite@live.cn>
2014-04-13 16:55:05 -0700

    quick and dirty implementation of VBIndex passed small test sets. 1. writePosting: first write termId, then number of bytes, then the whole list 2. readPosting: first read termId, then number of bytes, then the whole list
    TODO:
    1. may comes up with better serialization method
    2. mixed use of List<Integer> and int[] is really messy, is there a
    better way to do the dirty work?

--- cs276-pa1/src/cs276/assignments/VBIndex.java
+++ cs276-pa1/src/cs276/assignments/VBIndex.java
@@ -1,17 +1,20 @@
 package cs276.assignments;
 
+import javax.print.DocFlavor;
 import java.io.IOError;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class VBIndex implements BaseIndex {
 
     private static final int Integer_BYTES = Integer.SIZE / Byte.SIZE;
+    private static final int INVALID_VBCODE = -1;
 
-    private static void GapEncode(Integer[] inputDocIdsOutputGaps) {
+    private static void GapEncode(int[] inputDocIdsOutputGaps) {
         int curr = 0, prev;
         for (int i = 0; i < inputDocIdsOutputGaps.length; i++) {
             prev = curr;
@@ -20,23 +23,36 @@ public class VBIndex implements BaseIndex {
         }
     }
 
-    private static void VBEncode(Integer[] inputGaps, int numGaps, FileChannel fc) {
+    public static void GapDecode(int[] inputGapsOutputDocIds) {
+        for (int i = 1; i < inputGapsOutputDocIds.length; i++) {
+            inputGapsOutputDocIds[i] = inputGapsOutputDocIds[i - 1] + inputGapsOutputDocIds[i];
+        }
+    }
+
+
+    private static void VBEncode(int[] inputGaps, int numGaps, FileChannel fc) {
+        int totalNumBytes = 0;
         byte encodedInt[] = new byte[Integer.SIZE/7 + 1];
-        ByteBuffer buffer = ByteBuffer.allocate(numGaps * (Integer.SIZE/7 + 1) + Integer_BYTES);
-        buffer.putInt(numGaps);
+        ByteBuffer[] buffer = new ByteBuffer[2];
+        buffer[0] = ByteBuffer.allocate(Integer_BYTES); // stores total number of bytes
+        buffer[1] = ByteBuffer.allocate(numGaps * (Integer.SIZE/7 + 1) + Integer_BYTES); // stores postings
         for (int i = 0; i < numGaps; i++) {
             int numBytes = VBEncodeInteger(inputGaps[i], encodedInt);
+            totalNumBytes += numBytes;
             for (int j = 0; j < numBytes; j++) {
-                buffer.put(encodedInt[j]);
+                buffer[1].put(encodedInt[j]);
             }
         }
-        buffer.flip();
+        buffer[0].putInt(totalNumBytes);
+        buffer[0].flip();
+        buffer[1].flip();
         try {
             fc.write(buffer);
         } catch(IOException e) {
             e.printStackTrace();
         } finally {
-            buffer.clear();
+            buffer[0].clear();
+            buffer[1].clear();
         }
 
     }
@@ -60,17 +76,100 @@ public class VBIndex implements BaseIndex {
         return numBytes;
     }
 
+    private static void VBDecodeInteger(byte[] inputVBCode, int startIndex, int[] numberEndIndex) {
+        // Fill in your code here
+        int nextIndex = startIndex;
+        numberEndIndex[0] = 0;
+        Boolean success = false;
+        for (int i = 0; i < 5; i++) {
+            numberEndIndex[0] <<=7;
+            numberEndIndex[0] += (inputVBCode[nextIndex] & 0x7f);
+            if ((inputVBCode[nextIndex++] & 0x80) != 0) {
+                success = true;
+                break;
+            }
+        }
+        if (!success) {
+            numberEndIndex[0] = INVALID_VBCODE;
+            numberEndIndex[1] = startIndex;
+        } else {
+            numberEndIndex[1] = nextIndex;
+        }
+    }
+
+
     @Override
     public PostingList readPosting(FileChannel fc) {
         /*
          * Your code here
          */
+        ByteBuffer[] buffer = new ByteBuffer[3];
+        buffer[0] = ByteBuffer.allocate(Integer_BYTES);
+        buffer[1] = ByteBuffer.allocate(Integer_BYTES);
+        try {
+            fc.read(buffer[0]);
+            buffer[0].rewind();
+            fc.read(buffer[1]);
+            buffer[1].rewind();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int termId = buffer[0].getInt();
+        buffer[0].clear();
+        int numBytes = buffer[1].getInt();
+        buffer[1].clear();
+        buffer[2] = ByteBuffer.allocate(numBytes);
+        try {
+            fc.read(buffer[2]);
+            buffer[2].rewind();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // count the number of gaps
+        int numGaps = 0;
+        byte[] inputVBCode = new byte[numBytes];
+        byte b;
+        for (int i = 0; i < numBytes; i++) {
+            b = buffer[2].get();
+            inputVBCode[i] = b;
+            if ((b & 0x80) != 0) {
+                numGaps++;
+            }
+        }
+        int[] gaps = new int[numGaps];
+        int[] numberEndIndex = new int[2];
+        numberEndIndex[1] = 0;
+        for (int i = 0; i < numGaps; i++) {
+            VBDecodeInteger(inputVBCode, numberEndIndex[1], numberEndIndex);
+            gaps[i] = numberEndIndex[0];
+        }
+        GapDecode(gaps);
+        List<Integer> l = new ArrayList<Integer>();
+        for (int i = 0; i < numGaps; i++) {
+            l.add(gaps[i]);
+        }
+        if (numGaps > 0) {
+            return new PostingList(termId, l);
+        }
         return null;
     }
 
     @Override
     public void writePosting(FileChannel fc, PostingList p) {
-        Integer[] arr = p.getList().toArray(new Integer[p.getList().size()]);
+        // first write termId
+        ByteBuffer buffer = ByteBuffer.allocate(Integer_BYTES);
+        buffer.putInt(p.getTermId());
+        buffer.flip();
+        try {
+            fc.write(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int[] arr = new int[p.getList().size()];
+        Iterator<Integer> iter = p.getList().iterator();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = iter.next();
+        }
         GapEncode(arr);
         VBEncode(arr, arr.length, fc);
     }
